e762989d899c9654acf4459e77b8cc72da57aa0d
JayAnders1 <justinjay615@gmail.com>
2024-06-25 22:30:37 -0400

    second commit

--- advanced_slicing_techniques.py
+++ advanced_slicing_techniques.py
@@ -6,6 +6,8 @@ temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92,
 second_week = temperatures[7:14]
 print(second_week)
 
+
+#Question 3 task 2
 # over_100 = temperatures[-7:-1] not sure why this does not work here. I thought
 # that "-1" when slicing referred to the last item in a list, but for some reason -7 and -1 are leaving out the last number, 106.
 
